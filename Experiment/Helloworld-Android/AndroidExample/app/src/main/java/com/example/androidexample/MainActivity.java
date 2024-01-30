3669c2cfbc8acd77e5aced37d23471f0df74a9e6
Casper Run <casperr@iastate.edu>
2024-01-29 21:10:31 -0600

    No idea what this is

--- Experiment/Helloworld-Android/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiment/Helloworld-Android/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.style.BackgroundColorSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,7 +14,6 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     private TextView messageText;   // define message textview variable
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
         /* initialize UI elements */
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
-        messageText.setText("Hello There! General Kenobi! You are a bold one!");
+        messageText.setText("Hello there! General Kenobi! You are a bold one!");
+        messageText.setTextColor(0x00D800FF);
+
     }
 }
