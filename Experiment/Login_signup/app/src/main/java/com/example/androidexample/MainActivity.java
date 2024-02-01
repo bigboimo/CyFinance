8d9f099356ac0ec8be0eb40502319465ffb58a0d
Casper Run <casperr@iastate.edu>
2024-02-01 17:05:27 -0600

    Added a signout button to the login experiment

--- Experiment/Login_signup/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiment/Login_signup/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView messageText;   // define message textview variable
     private TextView usernameText;  // define username textview variable
     private Button loginButton;     // define login button variable
-    private Button signupButton;    // define signup button variable
+    private Button signupButton;
+    private Button logoutButton; // define signup button variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +27,20 @@ public class MainActivity extends AppCompatActivity {
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
         usernameText = findViewById(R.id.main_username_txt);// link to username textview in the Main activity XML
         loginButton = findViewById(R.id.main_login_btn);    // link to login button in the Main activity XML
-        signupButton = findViewById(R.id.main_signup_btn);  // link to signup button in the Main activity XML
+        signupButton = findViewById(R.id.main_signup_btn);
+        logoutButton = findViewById(R.id.main_logout_btn);// link to signup button in the Main activity XML
 
         /* extract data passed into this activity from another activity */
         Bundle extras = getIntent().getExtras();
         if(extras == null) {
             messageText.setText("Home Page");
-            usernameText.setVisibility(View.INVISIBLE);             // set username text invisible initially
+            usernameText.setVisibility(View.INVISIBLE);
+            logoutButton.setVisibility(View.INVISIBLE);// set username text invisible initially
         } else {
             messageText.setText("Welcome");
             usernameText.setText(extras.getString("USERNAME")); // this will come from LoginActivity
-            loginButton.setVisibility(View.INVISIBLE);              // set login button invisible
+            loginButton.setVisibility(View.INVISIBLE);
+            logoutButton.setVisibility(View.VISIBLE);// set login button invisible
             signupButton.setVisibility(View.INVISIBLE);             // set signup button invisible
         }
 
@@ -58,6 +62,17 @@ public class MainActivity extends AppCompatActivity {
 
                 /* when signup button is pressed, use intent to switch to Signup Activity */
                 Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when login button is pressed, use intent to switch to Login Activity */
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
