2351543ab970db664f22b096035bdf57f303561f
HUSSEIN BADERDDEINE <husseinb@iastate.edu>
2024-02-02 16:14:02 -0600

    adding xml file modifications

--- Experiment/hussein/exp1/tutorials-android_unit1_1_helloworld/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/androidTest/java/com/example/androidexample/ExampleInstrumentedTest.java
+++ Experiment/hussein/exp1/tutorials-android_unit1_1_helloworld/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/androidTest/java/com/example/androidexample/ExampleInstrumentedTest.java
@@ -22,5 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.androidexample", appContext.getPackageName());
+        System.out.println("Hellopeople");
     }
 }
