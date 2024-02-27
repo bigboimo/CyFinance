ccb523949e516c0f5e00bafd82d532c0b760d784
James Joseph <Jososeph@iastate.edu>
2024-02-26 18:45:03 -0600

    Added net worth endpoints (not tested)

---
+++ Backend/cy-finance/src/main/java/com/example/demo/netWorth/NetWorthController.java
@@ -0,0 +1,59 @@
+package com.example.demo.netWorth;
+
+import com.example.demo.util.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class NetWorthController {
+
+    @Autowired
+    NetWorthRepository netWorthRepository;
+
+    @GetMapping(path = "/networth")
+    List<NetWorth> getNetWorth() {
+        return netWorthRepository.findAll();
+    }
+
+    @GetMapping(path = "/networth/{id}")
+    NetWorth getNetWorthById(@PathVariable int id) {
+        return netWorthRepository.findById(id);
+    }
+
+    @PostMapping(path = "/networth")
+    String createNetWorth(@RequestBody NetWorth netWorth) {
+        Response<String> response = new Response<>();
+        if (netWorth == null) {
+            response.put("message", "No net worth specified");
+        } else {
+            response.put("message", "Net worth created");
+            netWorthRepository.save(netWorth);
+        }
+        return response.toString();
+    }
+
+    @PutMapping(path = "/networth")
+    String modifyNetWorth(@RequestBody NetWorth netWorth) {
+        Response<String> response = new Response<>();
+        if (netWorth == null) {
+            response.put("message", "No net worth provided");
+        } else if (netWorthRepository.findById(netWorth.getId()) == null) {
+            response.put("message", "Net worth doesn't exist");
+        } else {
+            netWorthRepository.save(netWorth);
+            response.put("message", "Net worth modified");
+        }
+        return response.toString();
+    }
+
+    @DeleteMapping(path = "/networth/{id}")
+    String deleteNetWorth(@RequestParam int id) {
+        Response<String> response = new Response<>();
+        netWorthRepository.deleteById(id);
+        response.put("message", "Net worths deleted");
+        return response.toString();
+    }
+
+}
