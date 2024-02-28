59415c26348e9f1f9e3bc392afa774679993dab7
Casper Run <casperr@iastate.edu>
2024-02-28 00:27:41 -0600

    Created a new CyFinance app project with a dashboard template. This will be our new hub for frontend

--- Frontend/CyFinance/app/src/main/java/com/example/cyfinance/LoginActivity.java
+++ Frontend/CyFinance/app/src/main/java/com/example/cyfinance/LoginActivity.java
@@ -1,6 +1,4 @@
-package com.example.androidexample;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.cyfinance;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,23 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
-
-
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.example.cyfinance.ui.home.HomeFragment;
 
 public class
 LoginActivity extends AppCompatActivity {
@@ -56,7 +40,7 @@ LoginActivity extends AppCompatActivity {
                 String password = passwordEditText.getText().toString();
 
                 /* when login button is pressed, use intent to switch to Login Activity */
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                Intent intent = new Intent(LoginActivity.this, HomeFragment.class);
                 intent.putExtra("USERNAME", username);  // key-value to pass to the MainActivity
                 intent.putExtra("PASSWORD", password);  // key-value to pass to the MainActivity
                 startActivity(intent);  // go to MainActivity with the key-value data
