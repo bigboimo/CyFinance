383dc2bb58bc30233f2e617ac9e09ad182d3446e
Casper Run <casperr@iastate.edu>
2024-02-27 22:50:06 -0600

    Added volley component to the Login Activity code still working on getting a post request working

--- Frontend/LoginRegister/Login_signup/app/src/main/java/com/example/androidexample/LoginActivity.java
+++ Frontend/LoginRegister/Login_signup/app/src/main/java/com/example/androidexample/LoginActivity.java
@@ -8,6 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class
 LoginActivity extends AppCompatActivity {
 
@@ -16,6 +34,7 @@ LoginActivity extends AppCompatActivity {
     private Button loginButton;         // define login button variable
     private Button signupButton;        // define signup button variable
 
+    private String url = ""
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,5 +74,12 @@ LoginActivity extends AppCompatActivity {
 
             }
         });
+
+        private void postRequest() {
+
+
+        }
+
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
     }
 }
--- Frontend/LoginRegister/Login_signup/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Frontend/LoginRegister/Login_signup/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -8,6 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
