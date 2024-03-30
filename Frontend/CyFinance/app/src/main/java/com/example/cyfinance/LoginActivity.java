c1ba8655683739fdfd8970acd7b20e6756245085
Casper Run <casperr@iastate.edu>
2024-03-30 12:50:01 -0500

    Changed the gradle script to try to print the javadoc correctly

--- Frontend/CyFinance/app/src/main/java/com/example/cyfinance/LoginActivity.java
+++ Frontend/CyFinance/app/src/main/java/com/example/cyfinance/LoginActivity.java
@@ -12,22 +12,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
-import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.example.cyfinance.ui.home.HomeFragment;
 
 public class
 LoginActivity extends AppCompatActivity {
