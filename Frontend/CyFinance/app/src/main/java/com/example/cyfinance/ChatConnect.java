903f51e412416b2b0f3bcc5799bd82bee144f56b
HUSSEIN BADERDDEINE <husseinb@iastate.edu>
2024-04-02 18:22:27 -0500

    Chat feature push and fixed earnings and expenses pages

---
+++ Frontend/CyFinance/app/src/main/java/com/example/cyfinance/ChatConnect.java
@@ -0,0 +1,44 @@
+package com.example.cyfinance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ChatConnect extends AppCompatActivity{
+    String url= "ws://10.10.2.2:8080/chat/1/Hussein";
+    private Button connectBtn, connectBtn2, backBtn, backBtn2;
+    private EditText serverEtx, usernameEtx, serverEtx2, usernameEtx2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chatconnect);
+        WebSocketManager.getInstance().init(this);
+        /* initialize UI elements */
+        connectBtn = (Button) findViewById(R.id.connectBtn);
+        serverEtx = (EditText) findViewById(R.id.serverEdt);
+        usernameEtx = (EditText) findViewById(R.id.unameEdt);
+
+
+        /* connect button listener */
+        connectBtn.setOnClickListener(view -> {
+            String serverUrl = serverEtx.getText().toString() + usernameEtx.getText().toString();
+            String url = "ws://10.10.2.2:8080/chat/1/Hussein";
+            // Establish WebSocket connection and set listener
+            WebSocketManager.getInstance().connectWebSocket(url);
+
+            // got to chat activity
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        });
+
+
+        /* back button listener */
+
+        /* back button listener */
+
+    }
+}
