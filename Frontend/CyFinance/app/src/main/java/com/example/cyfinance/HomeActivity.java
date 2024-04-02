b1c50c061eb5e97af00cd167eb2d38793cb1f7d1
Casper Run <casperr@iastate.edu>
2024-04-01 22:04:21 -0500

    Finished the home page xml and started on the get request

--- Frontend/CyFinance/app/src/main/java/com/example/cyfinance/HomeActivity.java
+++ Frontend/CyFinance/app/src/main/java/com/example/cyfinance/HomeActivity.java
@@ -2,8 +2,14 @@ package com.example.cyfinance;
 
 import android.os.Bundle;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.example.cyfinance.ui.Admin.AdminActivity;
 import com.example.cyfinance.ui.Earnings.EarningsDActivity;
+
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,19 +20,41 @@ import androidx.annotation.NonNull;
 
 
 import com.example.cyfinance.ui.Expenses.ExpensesDActivity;
+import com.example.cyfinance.util.Constants;
+import com.example.cyfinance.util.JsonRequest;
+import com.example.cyfinance.util.SessionManager;
 import com.google.android.material.navigation.NavigationBarView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
+    TextView totalNetworth;
+    TextView totalAssets;
+    TextView totalLiabilities;
+    String Response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        NavigationBarView navView = findViewById(R.id.nav_view);
 
+        TextView totals = findViewById(R.id.text_totals);
+        totalNetworth = findViewById(R.id.text_networth);
+
+        totalAssets = findViewById(R.id.text_assets);
+
+        totalLiabilities = findViewById(R.id.text_liabilities);
+
+        NavigationBarView navView = findViewById(R.id.nav_view);
         navView.setSelectedItemId(R.id.navigation_home);
+
 
         navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -51,6 +79,54 @@ public class HomeActivity extends AppCompatActivity {
                 return false;
             }
         });
+        private void getRequest() {
 
+            // Convert input to JSONObject
+            JSONObject postBody = null;
+
+            JsonRequest request = new JsonRequest(
+                    Request.Method.GET,
+                    Constants.URL + "/networth",
+                    postBody,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+
+                            } catch (JSONException e) {
+                                System.out.println(e);
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            //Response = error.getMessage();
+                            Response = error.toString();
+                            System.out.println(error);
+
+                        }
+                    }
+            ) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> headers = new HashMap<String, String>();
+                    //                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+                    //                headers.put("Content-Type", "application/json");
+                    return headers;
+                }
+
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    //params.put();
+
+                    return params;
+                }
+            };
+
+            // Adding request to request queue
+            VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+        }
     }
 }
