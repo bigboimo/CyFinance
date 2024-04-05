5114ff743178ad4be7ebe60627e718ba32556645
HUSSEIN BADERDDEINE <husseinb@iastate.edu>
2024-04-05 14:31:28 -0500

    Earnings has put,post

--- Frontend/CyFinance/app/src/main/java/com/example/cyfinance/ui/Earnings/EarningsDActivity.java
+++ Frontend/CyFinance/app/src/main/java/com/example/cyfinance/ui/Earnings/EarningsDActivity.java
@@ -2,87 +2,106 @@ package com.example.cyfinance.ui.Earnings;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.example.cyfinance.ChatConnect;
+import com.example.cyfinance.HomeActivity;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.example.cyfinance.EarningsActivity;
-import com.example.cyfinance.ExpensesActivity;
+import com.example.cyfinance.LoginActivity;
 import com.example.cyfinance.R;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.cyfinance.HomeActivity;
 import com.example.cyfinance.VolleySingleton;
 import com.example.cyfinance.ui.Admin.AdminActivity;
-
-import android.view.MenuItem;
-import android.widget.EditText;
-
+import com.example.cyfinance.ui.Change.LiabilityChange;
 import com.example.cyfinance.ui.Expenses.ExpensesDActivity;
+import com.example.cyfinance.util.Constants;
+import com.example.cyfinance.util.JsonRequest;
+import com.example.cyfinance.util.SessionManager;
 import com.google.android.material.navigation.NavigationBarView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class EarningsDActivity extends AppCompatActivity {
 
-    private Button testButton;
-    private EditText
+    private EditText primaryIncomeEditText;  // define username edittext variable
+    private EditText secondaryIncomeEditText;  // define password edittext variable
 
-    private EditText
+    String primary;
+    String secondary;
 
-    public void onCreate(Bundle savedInstanceState) {
+    String primary1;
+
+    String secondary1;
+    String id;
+    String Response;
+    String Earnings;
+    // user Id set by cookie on login
+    String userId;
+    Button Submit;
+
+    // User session for other classes to track
+    SessionManager session;
+    boolean success = false;
+    private String url = Constants.URL + "/earnings";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_earningsd);
+        setContentView(R.layout.activity_earningsd);            // link to Login activity XML
 
-        NavigationBarView navView = findViewById(R.id.nav_view);
+        /* initialize UI elements */
+        primaryIncomeEditText = findViewById(R.id.primary);
+        secondaryIncomeEditText = findViewById(R.id.secondary);
+        // define login button variable
+        Button loginButton = findViewById(R.id.submit);    // link to login button in the Login activity XML
+        // define signup button variable
 
-        navView.setSelectedItemId(R.id.navigation_earnings);
-
-
-        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        /* click listener on login button pressed */
+        Submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                switch (item.getItemId()) {
-                    case R.id.navigation_earnings:
-                        return true;
-                    case R.id.navigation_home:
-                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.navigation_admin:
-                        startActivity(new Intent(getApplicationContext(), AdminActivity.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.navigation_expenses:
-                        startActivity(new Intent(getApplicationContext(), ExpensesDActivity.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                }
-                return false;
+            public void onClick(View v) {
+                /* grab strings from user inputs */
+                primary = primaryIncomeEditText.getText().toString();
+                secondary = secondaryIncomeEditText.getText().toString();
+                postRequest();
+                Intent intent = new Intent(EarningsDActivity.this, ExpensesDActivity.class);
+                startActivity(intent);  // go to expenses
             }
         });
-        private void postRequest() {
-            JSONObject postBody = new JSONObject();
-            try {
-                postBody.put("primaryMonthlyIncome", );
-                postBody.put("secondaryMonthlyIncome", );
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
 
+        /* click listener on signup button pressed */
+
+
+
+
+
+    private void postRequest() {
+//
+//        try {
+//            // Assuming 'userId' is already set. Ensure it's the correct user ID for which earnings are to be fetched.
+//            postParams.put("id", userId);
+//            // Optionally, add primaryMonthlyIncome and secondaryMonthlyIncome if required by the backend, though it seems unusual for a fetch operation.
+//        } catch (JSONException e) {
+//            Toast.makeText(EarningsDActivity.this, "Error creating request payload.", Toast.LENGTH_SHORT).show();
+//            e.printStackTrace();
+//            return;
+//        }
+            JSONObject postBody = null;
             JsonObjectRequest request = new JsonObjectRequest(
                     Request.Method.POST,
-                    url,
+                    Constants.URL + "/earnings",
                     postBody,
                     new Response.Listener<JSONObject>() {
                         @Override
@@ -97,7 +116,7 @@ public class EarningsDActivity extends AppCompatActivity {
                             System.out.println(response);
                             if(Response.equals("success")){
                                 System.out.println("Data saved");
-                                Intent intent = new Intent(EarningsActivity.this, ExpensesActivity.class);
+                                Intent intent = new Intent(EarningsDActivity.this, ExpensesDActivity.class);
                                 startActivity(intent);
                             }
                             else{
@@ -123,75 +142,60 @@ public class EarningsDActivity extends AppCompatActivity {
                     return headers;
                 }
 
-                @Override
-                protected Map<String, String> getParams() {
-                    Map<String, String> params = new HashMap<String, String>();
-                    params.put("PrimaryMonthlyIncome", primary);
-                    params.put("SecondaryMonthlyIncome", secondary);
-                    return params;
+        };
+
+    }
+
+
+    private void putRequest() {
+
+        // Convert input to JSONObject
+        JSONObject postBody = null;
+
+        JsonRequest request = new JsonRequest(
+                Request.Method.PUT,
+                Constants.URL + "/earnings/" +   primary1 + secondary1,
+                postBody,
+                new com.android.volley.Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            System.out.println("[Login] HTTP Response: " + response);
+                            JSONArray data = response.getJSONArray("data");
+                            JSONObject headers = response.getJSONObject("headers");
+                            Response = data.getJSONObject(0).getString("message");
+
+                            if (Response != null && Response.equals("User modified")) {
+                                Intent intent = new Intent(EarningsDActivity.this, ExpensesDActivity.class);
+                                startActivity(intent);
+                            }
+                        } catch (JSONException e) {
+                            System.out.println(e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //Response = error.getMessage();
+                        Response = error.toString();
+                        System.out.println(error);
+
+                    }
                 }
-            };
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("", "" + session.getUserDetails().get("id"));
+                return headers;
+            }
 
-            VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
-        }
-    }
 
 
-//        if (getActivity() != null) {
-//            context = getActivity().getApplicationContext();
-//        } else {
-//            System.out.println("No valid activity");
-//            Intent intent = new Intent(getContext(), LoginActivity.class);
-//            startActivity(intent);
-//        }
-//
-//        session = new SessionManager(context);
+    };
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+        // Adding request to request queue
 
-//        System.out.println("User id " + session.getUserDetails());
-
-//        final TextView textView = binding.textHome;
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
-//        // Example button for sending a request with session headers
-//        testButton = binding.testBtn;
-//
-//        testButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                System.out.println("Test clicked");
-//                JsonRequest request = new JsonRequest(
-//                        Request.Method.GET,
-//                        Constants.URL + "/users", new JSONObject(),
-//                        new Response.Listener<JSONObject>() {
-//                            @Override
-//                            public void onResponse(JSONObject response) {
-//                                System.out.println("[Testing] HTTP Response: " + response);
-//                            }
-//                        },
-//                        new Response.ErrorListener() {
-//                            @Override
-//                            public void onErrorResponse(VolleyError error) {
-//                                System.out.println(error);
-//
-//                            }
-//                        }
-//                ) {
-//
-//                    /**
-//                     * Passing some request headers
-//                     */
-//                    @Override
-//                    public Map<String, String> getHeaders() throws AuthFailureError {
-//                        HashMap<String, String> headers = new HashMap<String, String>();
-//                        headers.put("Cookie", "user-id=" + session.getUserDetails().get("id"));
-//                        return headers;
-//                    }
-//                };
-//                // Adding request to request queue
-//                VolleySingleton.getInstance(context).addToRequestQueue(request);
-//            }
-//        });
-
-//        return root;
-    }
+}
 }
